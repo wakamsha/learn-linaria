c8c2544054d5f7776353b7ad4ad024cb01c34237
wakamsha <wakamsha@gmail.com>
2023-03-08 02:16:21 +0900

    :sparkles: app パッケージを用意する

---
+++ packages/app/vite.config.ts
@@ -0,0 +1,12 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+});

46e6a1fc56d0f1f9025162bc816597cacb34e8a5
wakamsha <wakamsha@gmail.com>
2023-03-10 00:55:25 +0900

    :sparkles: app に linaria を導入する

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,12 +1,13 @@
+import linaria from '@linaria/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [react()],
   server: {
     port: 3000,
     open: true,
   },
+  plugins: [react(), linaria()],
 });

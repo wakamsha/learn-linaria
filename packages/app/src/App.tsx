f86153a2bb12fc02b40919dadd0e85792ad0367d
YAMADA Naoki <wakamsha@gmail.com>
2023-03-10 00:42:47 +0900

    :recycle: app を最小構成で再実装する (#1)

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import './App.css';
-import reactLogo from './assets/react.svg';
 
 export const App = () => {
   const [count, setCount] = useState(0);
@@ -10,9 +8,6 @@ export const App = () => {
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>

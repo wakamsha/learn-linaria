0ec819dd078053dc9f0ccce4cfd168c927a5f033
wakamsha <wakamsha@gmail.com>
2023-03-21 22:04:25 +0900

    vite によるビルドロジックを `builder` パッケージとして切り出す

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,38 +1,17 @@
-import linaria from '@linaria/vite';
-import react from '@vitejs/plugin-react';
+import { createUserConfig } from '@learn-linaria/builder';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 
 const alias = {
   '@learn-linaria/core': resolve(__dirname, '../core/src'),
 };
 
 // eslint-disable-next-line import/no-default-export
-export default defineConfig({
-  server: {
-    port: 3000,
-    open: true,
-  },
-  resolve: {
+export default defineConfig(({ mode }): UserConfig => {
+  console.info({ mode });
+
+  return createUserConfig({
+    basePath: __dirname,
     alias,
-  },
-  plugins: [
-    react(),
-    linaria({
-      sourceMap: true,
-      babelOptions: {
-        plugins: [
-          // linaria にエイリアスパスを認識させるための措置。
-          [
-            'module-resolver',
-            {
-              alias,
-            },
-          ],
-        ],
-        // linaria のスタイル定義内で外部ファイルから import した値が型情報ついてるとパースエラーになるので追加
-        presets: ['@babel/preset-typescript'],
-      },
-    }),
-  ],
+  });
 });
---
+++ packages/builder/dist/index.d.ts
@@ -0,0 +1,20 @@
+import type { BuildOptions, UserConfig } from 'vite';
+
+type Props = {
+  basePath: string;
+  /**
+   * @default 3000
+   */
+  port?: number;
+  /**
+   * @default {}
+   */
+  define?: Record<string, unknown>;
+  /**
+   * @default {}
+   */
+  build?: BuildOptions;
+  alias?: Record<string, string>;
+};
+export declare function createUserConfig({ basePath, port, define, build, alias }: Props): UserConfig;
+export {};
